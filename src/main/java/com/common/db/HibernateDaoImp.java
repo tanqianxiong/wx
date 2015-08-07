c893732eb3fcfa0d1837f14a4cff0fec7f80ad20
fengzhimengna <yefeng1011@qq.com>
2015-08-07 10:49:12 +0800

    增加了多字段or模糊查询，并修改所有的方法名以do开头

--- src/main/java/com/common/db/HibernateDaoImp.java
+++ src/main/java/com/common/db/HibernateDaoImp.java
@@ -2,14 +2,20 @@ package com.common.db;
 
 import java.lang.reflect.ParameterizedType;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 
+import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Expression;
+import org.hibernate.criterion.LogicalExpression;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.SimpleExpression;
 import org.springframework.dao.DataAccessResourceFailureException;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
@@ -59,7 +65,7 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 	 * @param orderProps
 	 * @return
 	 */
-	public String genOrderHql(String hql, Map<String, String> orderProps) {
+	public String doGenOrderHql(String hql, Map<String, String> orderProps) {
 		StringBuffer orderStr = new StringBuffer();
 		orderStr.append(hql);
 		if (orderProps != null && orderProps.size() != 0) {
@@ -95,7 +101,7 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 	/*
 	 * 插入一个实体
 	 */
-	public String insert(T entity) {
+	public String doInsert(T entity) {
 		if (entity.getClass().getCanonicalName().equals(this.entityName)) {
 			return this.getHibernateTemplate().save(entity).toString();
 		} else {
@@ -106,7 +112,7 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 	/*
 	 * 修改一个实体
 	 */
-	public void update(T entity) {
+	public void doUpdate(T entity) {
 //		if (entity.getClass().getCanonicalName().equals(this.entityName)) {
 			this.getHibernateTemplate().update(entity);
 //		} else {
@@ -114,7 +120,7 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 //		}
 	}
 	
-	public int updateProperty(String id,String propertyName,Object value) {
+	public int doUpdateProperty(String id,String propertyName,Object value) {
 		String hql = "update "+this.entityName+" set "+propertyName+" = ? where id = ?";
 		return this.getHibernateTemplate().bulkUpdate(hql,value,id);
 	}
@@ -122,12 +128,12 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 	/*
 	 * 删除一个实体
 	 */
-	public void delete(T entity) {
+	public void doDelete(T entity) {
 		this.getHibernateTemplate().delete(entity);		
 
 	}
 
-	public void deleteById(Object propertyValue) {
+	public void doDeleteById(Object propertyValue) {
 		String hql = "delete " + this.entityName + " _t001 where _t001.id=?";
 		logger.info(hql);
 		this.getHibernateTemplate().bulkUpdate(hql,
@@ -138,7 +144,7 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 	/*
 	 * 根据属性删除实体
 	 */
-	public void deleteByProperty(String propertyName, Object propertyValue) {
+	public void doDeleteByProperty(String propertyName, Object propertyValue) {
 		String hql = "delete " + this.entityName + " _t001 where _t001."
 				+ propertyName + "=?";
 		logger.info(hql);
@@ -150,7 +156,7 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 	/*
 	 * 根据属性集合删除实体
 	 */
-	public void deleteByProperties(Map<String, Object> param) {
+	public void doDeleteByProperties(Map<String, Object> param) {
 		StringBuffer keys = new StringBuffer();
 		Object[] values = new Object[param.keySet().size()];
 		int i = 0;
@@ -169,13 +175,13 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 	 * 根据id得到实体
 	 */
 	@SuppressWarnings("unchecked")
-	public T getById(String id) {
+	public T doGetById(String id) {
 		return (T) this.getHibernateTemplate().get(this.entityName, id);
 	}
 
-	public T getByProperty(String propertyName, String propertyValue) {
+	public T doGetByProperty(String propertyName, String propertyValue) {
 		List<T> list = this
-				.getListByProperty(propertyName, propertyValue, null);
+				.doGetListByProperty(propertyName, propertyValue, null);
 		if (list.size() > 0) {
 			return list.get(0);
 		} else {
@@ -184,10 +190,10 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 	}
 	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public T getByProperties(Map<String, Object> param,
+	public T doGetByProperties(Map<String, Object> param,
 			Map<String, String> orderProps)
 	{
-		List list = this.getListByProperties(param, orderProps);
+		List list = this.doGetListByProperties(param, orderProps);
 		if (list.size() > 0) {
 			return (T)list.get(0);
 		} else {
@@ -200,22 +206,22 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 	 * 根据实体属性得到实体列表
 	 */
 	@SuppressWarnings({ "unchecked" })
-	public List<T> getListByProperty(String propertyName, Object propertyValue,
+	public List<T> doGetListByProperty(String propertyName, Object propertyValue,
 			Map<String, String> orderProps) {
 		String hql = "from " + this.entityName + " _t001 where _t001."
 				+ propertyName + "=?";
-		hql = this.genOrderHql(hql, orderProps);
+		hql = this.doGenOrderHql(hql, orderProps);
 		logger.info(hql);
 		return (List<T>) this.getHibernateTemplate().find(hql, propertyValue);
 	}
 	
 	@SuppressWarnings("unchecked")
-	public List<T> getListByNullProperty(String propertyName,
+	public List<T> doGetListByNullProperty(String propertyName,
 			Map<String, String> orderProps) {
 
 		String hql = "from " + this.entityName + " _t001 where _t001."
 				+ propertyName + " is null ";
-		hql = this.genOrderHql(hql, orderProps);
+		hql = this.doGenOrderHql(hql, orderProps);
 		logger.info(hql);
 		return (List<T>) this.getHibernateTemplate().find(hql);
 	}
@@ -223,7 +229,7 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 	/*
 	 * 根据实体属性得到记录数
 	 */
-	public int getEntityCountByProperty(String propertyName,
+	public int doGetEntityCountByProperty(String propertyName,
 			Object propertyValue) {
 		String hql = "select count(_t001) from " + this.entityName
 				+ " _t001 where _t001." + propertyName + "=?";
@@ -238,12 +244,12 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 	 * 根据实体属性得到一页记录
 	 */
 	@SuppressWarnings("rawtypes")
-	public List getEntityListByProperty(String propertyName,
+	public List doGetEntityListByProperty(String propertyName,
 			Object propertyValue, int start, int count,
 			Map<String, String> orderProps) {
 		String hql = "from " + this.entityName + " _t001 where _t001."
 				+ propertyName + "=?";
-		hql = this.genOrderHql(hql, orderProps);
+		hql = this.doGenOrderHql(hql, orderProps);
 		Query query = this.getSession().createQuery(hql);
 		query.setParameter(0, propertyValue);
 		query.setCacheable(useCache);
@@ -261,7 +267,7 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 	 * @return
 	 */
 	@SuppressWarnings("unchecked")
-	public List<T> getListByProperties(Map<String, Object> param,
+	public List<T> doGetListByProperties(Map<String, Object> param,
 			Map<String, String> orderProps) {
 		StringBuffer keys = new StringBuffer();
 		Object[] values = new Object[param.keySet().size()];
@@ -273,7 +279,7 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 		}
 		String hql = "from " + this.entityName + " _t001 where 1=1 "
 				+ keys.toString();
-		hql = this.genOrderHql(hql, orderProps);
+		hql = this.doGenOrderHql(hql, orderProps);
 		logger.info(hql);
 		return (List<T>) this.getHibernateTemplate().find(hql, values);
 	}
@@ -283,9 +289,9 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 	 * @param param
 	 * @return
 	 */
-	public int getCountByProperties(Map<String, Object> param) {
+	public int doGetCountByProperties(Map<String, Object> param) {
 		if (param == null) {
-			return this.getCount();
+			return this.doGetCount();
 		} else {
 			StringBuffer keys = new StringBuffer();
 			Object[] values = new Object[param.keySet().size()];
@@ -313,9 +319,9 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 	 * @param param
 	 * @return
 	 */
-	public int getCountByProperty(String property, Object value) {
+	public int doGetCountByProperty(String property, Object value) {
 		if (property == null || property.isEmpty()) {
-			return this.getCount();
+			return this.doGetCount();
 		} else {
 			StringBuffer keys = new StringBuffer();
 			keys.append("and _t001." + property + "=? ");
@@ -329,7 +335,7 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 		}
 	}
 
-	public int getCount() {
+	public int doGetCount() {
 		String hql = "select count(_t001) from " + this.entityName
 				+ " _t001 where 1=1 ";
 		Query query = this.getSession().createQuery(hql);
@@ -348,10 +354,10 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 	 * @return
 	 */
 	@SuppressWarnings("unchecked")
-	public List<T> getListByPage(Map<String, Object> paramMap, int start,
+	public List<T> doGetListByPage(Map<String, Object> paramMap, int start,
 			int count, Map<String, String> orderProps) {
 		if (paramMap == null) {
-			return this.getListByPage(start, count, orderProps);
+			return this.doGetListByPage(start, count, orderProps);
 		} else {
 			StringBuffer keys = new StringBuffer();
 			Object[] values = new Object[paramMap.keySet().size()];
@@ -363,7 +369,7 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 			}
 			String hql = "from " + this.entityName + " _t001 where 1=1 "
 					+ keys.toString();
-			hql = this.genOrderHql(hql, orderProps);
+			hql = this.doGenOrderHql(hql, orderProps);
 			Query query = this.getSession().createQuery(hql);
 			for (int j = 0; j < values.length; j++) {
 				query.setParameter(j, values[j]);
@@ -377,7 +383,7 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 	}
 
 	@SuppressWarnings("unchecked")
-	public List<T> getListByPage(int start, int count,
+	public List<T> doGetListByPage(int start, int count,
 			Map<String, String> orderProps) {
 		String hql = "from " + this.entityName + " _t001 where 1=1 ";
 		Query query = this.getSession().createQuery(hql);
@@ -388,8 +394,8 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 		return query.list();
 	}
 
-	public List<T> getAll() {
-		return this.getAll(null);
+	public List<T> doGetAll() {
+		return this.doGetAll(null);
 	}
 
 	/*
@@ -397,9 +403,9 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 	 */
 
 	@SuppressWarnings("unchecked")
-	public List<T> getAll(Map<String, String> orderProps) {
+	public List<T> doGetAll(Map<String, String> orderProps) {
 		String hql = "from " + this.entityName + " _t001";
-		hql = this.genOrderHql(hql, orderProps);
+		hql = this.doGenOrderHql(hql, orderProps);
 		logger.info(hql);
 		return (List<T>) this.getHibernateTemplate().find(hql);
 	}
@@ -407,7 +413,7 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 	/*
 	 * 得到所有实体集合记录数
 	 */
-	public int getEntityCountAll() {
+	public int doGetEntityCountAll() {
 		String hql = "select count(_t001) from " + this.entityName + " _t001";
 		Query query = this.getSession().createQuery(hql);
 		query.setCacheable(useCache);
@@ -419,9 +425,9 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 	 * 得到所有实体集合一页记录
 	 */
 	@SuppressWarnings("rawtypes")
-	public List getAll(int start, int count, Map<String, String> orderProps) {
+	public List doGetAll(int start, int count, Map<String, String> orderProps) {
 		String hql = "from " + this.entityName + " _t001";
-		hql = this.genOrderHql(hql, orderProps);
+		hql = this.doGenOrderHql(hql, orderProps);
 		Query query = this.getSession().createQuery(hql);
 		query.setCacheable(useCache);
 		query.setFirstResult(start);
@@ -429,6 +435,33 @@ public class HibernateDaoImp<T> extends HibernateDaoSupport {
 		logger.info(hql);
 		return query.list();
 	}
+	
+	@SuppressWarnings({ "unchecked", "deprecation" })
+	public List<T> doGetListByLikeProperties(Map<String, Object> listProp){
+		List<SimpleExpression> seList=new ArrayList<SimpleExpression>();
+		Criteria criteria = this.getSession(true).createCriteria(this.entityName);
+		if(listProp!=null && !listProp.isEmpty()){
+			for (String key : listProp.keySet()) {
+				seList.add(Restrictions.like(key,listProp.get(key)));
+			}
+		}
+		criteria.add(orExpressionLink(seList));
+		//criteria.add(Restrictions.or(seList.get(0),seList.get(1)));
+		return criteria.list();
+	}
 
-
+	/*
+	 * 递归函数
+	 */
+	private LogicalExpression orExpressionLink(List<SimpleExpression> seList){
+		if(seList.size()<3){
+			return Restrictions.or(seList.get(0),seList.get(1));
+		}
+		else{
+			//List<SimpleExpression> tmpList=new ArrayList<SimpleExpression>(seList.subList(1, seList.size()-1));
+			SimpleExpression se=seList.get(0);
+			seList.subList(0, 1).clear();
+			return Restrictions.or(se,orExpressionLink(seList));
+		}
+	}
 }
