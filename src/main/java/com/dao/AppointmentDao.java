043a606a501e3230050cbc459da2c5b8650828b9
fengzhimengna <yefeng1011@qq.com>
2015-08-25 10:00:02 +0800

    精简Appointment代码

--- src/main/java/com/dao/AppointmentDao.java
+++ src/main/java/com/dao/AppointmentDao.java
@@ -14,17 +14,6 @@ public interface AppointmentDao extends BaseDao<Appointment> {
 	public void delete(String id);
 	public void update(Appointment appointment);
 	public Appointment get(String id);
-	/*List<Appointment> getLikeProperty(Map<String, Object> map, Map<String, Object> map2);
-	int getNumByWelfare(Welfare welfare);
-	List<Appointment> getListByWelfare(Welfare wf);
-	public List<Appointment> getListByProperty(String propertyName, Object propertyValue);
-	
-	public List<Appointment> getPagination(int start, int count);
-	public int getCount();
-	//List<Appointment> getPaginationByWelfareID(String welfareID, Map<String, Object> like, Map<String, Object> and, int i, int itemsPerPage);
-	List<Appointment> getEntityListByWelfareID(String welfareID, Object propertyValue, int start, int count, Map<String, String> orderProps); 
-	*/
-	//int getCountByWelfareID(String welfareID,Object value);
 	public List<Appointment> getListByProperties(Map<String, Object> andProps, int startIndex, int itemsPerPage,
 			Map<String, String> orderMap);
 	public int getCountByProperty(String propKey, Object propValue);
--- src/main/java/com/service/AppointmentService.java
+++ src/main/java/com/service/AppointmentService.java
@@ -14,17 +14,6 @@ public interface AppointmentService {
 	public void delete(String id);
 	public void update(Appointment appointment);
 	public Appointment get(String id);
-	/*List<Appointment> getLikeProperty(Map<String, Object> map);
-	public List<Appointment> getLikeProperty(Map<String, Object> map, Map<String, Object> map2);
-	public int getNumByWelfare(Welfare welfare);
-	public List<Appointment> getListByWelfare(Welfare wf);
-	public List<Appointment> getListByProperty(String propertyName, Object propertyValue);
-	
-	public List<Appointment> getPagination(int start, int count);
-	public int getCount();
-	public 	List<Appointment> getEntityListByWelfareID(String welfareID, Object propertyValue, int start, int count, Map<String, String> orderProps); 
-
-	public int getCountByWelfareID(String welfareID,Object value);*/
 	public List<Appointment> getListByProperties(Map<String, Object> andProps, int startIndex, int itemsPerPage,
 			Map<String, String> orderMap);
 	public int getCountByProperty(String propKey, Object propValue);
