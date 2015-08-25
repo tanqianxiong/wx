54c6461ff2008dec1a7f374b250fcc1e125cd756
fengzhimengna <yefeng1011@qq.com>
2015-08-25 09:58:34 +0800

    恢复删除错误的文件

---
+++ src/main/java/com/common/db/BaseDao.java
@@ -0,0 +1,48 @@
+
+package com.common.db;
+
+import java.util.List;
+import java.util.Map;
+
+public interface BaseDao<T> {
+
+//	public List<T> getAll();
+//	
+//	public List<T> getAll(Map<String, String> orderProps);
+//
+//	public List<T> getListByProperty(String propertyName, Object propertyValue,
+//			Map<String, String> orderProps);
+//	public List<T> getListByNullProperty(String propertyName,
+//			Map<String, String> orderProps);
+//
+//	public List<T> getListByProperties(Map<String, Object> param,
+//			Map<String, String> orderProps);
+//
+//	public T getById(String id);
+//
+//	public T getByProperty(String propName, String propValue);
+//	
+//	public T getByProperties(Map<String, Object> param,
+//			Map<String, String> orderProps);
+//
+//	public int getCountByProperties(Map<String, Object> paramMap);
+//	
+//	public int getCountByProperty(String property, Object value);
+//
+//	public List<T> getListByPage(Map<String, Object> paramMap, int start,
+//			int count, Map<String, String> orderProps);
+//
+//	public String insert(T t);
+//
+//	public void update(T t);
+//	
+//	public int updateProperty(String id,String propertyName,Object value);
+//
+//	public void delete(T t);
+//
+//	public void deleteById(Object id);
+//
+//	public void deleteByProperty(String propertyName, Object id);
+//	
+//	public void deleteByProperties(Map<String,Object> paramMap);
+}
---
+++ src/main/java/com/common/db/BaseEntity.java
@@ -0,0 +1,5 @@
+package com.common.db;
+
+public class BaseEntity {
+
+}
